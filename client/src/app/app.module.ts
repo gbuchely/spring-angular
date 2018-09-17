f571303232f8738db28bf4b17dab1ff20ad72a75
gbuchely <guidobuchely@gmail.com>
2018-09-16 22:02:05 -0500

    Include additional functionalities of CRUD

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -11,11 +11,32 @@ import { MatButtonModule, MatCardModule, MatInputModule, MatListModule, MatToolb
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { GiphyService } from './shared/giphy/giphy.service';
+import { CarEditComponent } from './car-edit/car-edit.component';
+
+import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
+
+const appRoutes: Routes = [
+  { path: '', redirectTo: '/car-list', pathMatch: 'full' },
+  {
+    path: 'car-list',
+    component: CarListComponent
+  },
+  {
+    path: 'car-add',
+    component: CarEditComponent
+  },
+  {
+    path: 'car-edit/:id',
+    component: CarEditComponent
+  }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    CarListComponent
+    CarListComponent,
+    CarEditComponent
   ],
   imports: [
     BrowserModule,
@@ -25,7 +46,9 @@ import { GiphyService } from './shared/giphy/giphy.service';
     MatCardModule,
     MatInputModule,
     MatListModule,
-    MatToolbarModule
+    MatToolbarModule,
+    FormsModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [CarService, GiphyService],
   bootstrap: [AppComponent]
