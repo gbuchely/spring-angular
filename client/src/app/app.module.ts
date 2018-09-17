7f09e7e0c90bf5e1732ded73ba5fcaacb5dcb7db
gbuchely <guidobuchely@gmail.com>
2018-09-16 19:04:54 -0500

    Include GET-LIST functionality

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,14 +3,31 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import { CarService } from './shared/car/car.service';
+import { HttpClientModule } from '@angular/common/http';
+import { CarListComponent } from './car-list/car-list.component';
+
+import { MatButtonModule, MatCardModule, MatInputModule, MatListModule, MatToolbarModule } from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { GiphyService } from './shared/giphy/giphy.service';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CarListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatCardModule,
+    MatInputModule,
+    MatListModule,
+    MatToolbarModule
   ],
-  providers: [],
+  providers: [CarService, GiphyService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
